68c577649e59b1c9c6e30c761367f635d3dbc008
Redlinx <p.francisrey@gmail.com>
2013-10-26 01:13:53 +0800

    Updated content display

--- application/views/PDF_view.php
+++ application/views/PDF_view.php
@@ -13,30 +13,37 @@
 	{
 		$acct_username = "Username: ".$result[$x]->acct_username;
 	    $acct_password = "Password: ".utf8_decode($result[$x]->acct_password);
-		$acct_name = "Name: ".strtoupper(utf8_decode($result[$x]->acct_fName." ".
+		$acct_name = strtoupper(utf8_decode($result[$x]->acct_fName." ".
 													 $result[$x]->acct_mName." ".
 													 $result[$x]->acct_lName));
-		$course = "Course: ".$result[$x]->course_id;
+		$course = $result[$x]->course_id;
 		$dateTime_printed = "Date and Time Printed: ".unix_to_human($timestamp);	
 		$message_1 = "This document is CONFIDENTIAL.";
 		$message_2 = "Present this to the UIC COMELEC during the signing of clearance.";
 	    $paper_size = array(107.95,139.7); 	// Unit used is Millimeter
 
 	    $this->pdf->AddPage('P',$paper_size);
-	    $this->pdf->SetFont('Times','B',11);
-		$this->pdf->MultiCell(0,5, 'University of Immaculate Conception',0,'C');
+	    $this->pdf->SetFont('Times','',11);
+		$this->pdf->MultiCell(0,5, 'University of the Immaculate Conception',0,'C');
+		$this->pdf->SetFont('Times','B',11);
 		$this->pdf->MultiCell(0,5, 'Commission on Elections and Appointments',0,'C');
-		$this->pdf->Ln();
 		$this->pdf->SetFont('Times','',11);
-		$this->pdf->MultiCell(0,5, 'ACCESS DETAILS',0,'C');
+		$this->pdf->MultiCell(0,5, 'Student Supreme Government',0,'C');
+		
 		$this->pdf->Ln();
+		$this->pdf->SetFont('Times','U',11);
+		$this->pdf->MultiCell(0,5, 'VOTER PROFILE',0,'L');
+		$this->pdf->SetFont('Courier','B',10);
 		$this->pdf->MultiCell(0,5, $acct_name,0,'L');
-		$this->pdf->MultiCell(0,5, $course,0,'D');
-		$this->pdf->Ln();
+		$this->pdf->SetFont('Courier','',9);
+		$this->pdf->MultiCell(0,4, $course,0,'D');
+		$this->pdf->Ln(10);
+		$this->pdf->SetFont('Times','U',11);
+		$this->pdf->MultiCell(0,5, 'ACCESS DETAILS',0,'L');
 	    $this->pdf->SetFont('Courier','',10);
 	    $this->pdf->MultiCell(0,5, $acct_username,0,'L');
 	    $this->pdf->MultiCell(0,5, $acct_password,0,'L');
-		$this->pdf->Ln(15);
+		$this->pdf->Ln(10);
 		$this->pdf->SetFont('Courier','B',10);
 		$this->pdf->MultiCell(0,5,"Reminders",'','L');
 		$this->pdf->SetFont('Courier','',10);
