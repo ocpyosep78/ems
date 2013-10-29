210a3a2be7d7fdaba4e647e84c4d3c5f37ecc444
Redlinx <p.francisrey@gmail.com>
2013-10-29 09:49:36 +0800

    Fixed bugs -unselected courses -duplicated email -numeric username -change CI core error message for duplicated entry

--- application/views/PDF_view.php
+++ application/views/PDF_view.php
@@ -17,19 +17,19 @@
 													 $result[$x]->acct_mName." ".
 													 $result[$x]->acct_lName));
 		$course = $result[$x]->course_id;
-		$dateTime_printed = "Date and Time Printed: ".unix_to_human($timestamp);	
+		$dateTime_printed = "***Date_Time_Printed ".unix_to_human($timestamp);	
 		$message_1 = "This document is CONFIDENTIAL.";
 		$message_2 = "Present this to the UIC COMELEC during the signing of clearance.";
 	    $paper_size = array(107.95,139.7); 	// Unit used is Millimeter
 
 	    $this->pdf->AddPage('P',$paper_size);
+	    $this->pdf->Ln(2);
 	    $this->pdf->SetFont('Times','',11);
 		$this->pdf->MultiCell(0,5, 'University of the Immaculate Conception',0,'C');
 		$this->pdf->SetFont('Times','B',11);
 		$this->pdf->MultiCell(0,5, 'Commission on Elections and Appointments',0,'C');
 		$this->pdf->SetFont('Times','',11);
 		$this->pdf->MultiCell(0,5, 'Student Supreme Government',0,'C');
-		
 		$this->pdf->Ln();
 		$this->pdf->SetFont('Times','U',11);
 		$this->pdf->MultiCell(0,5, 'VOTER PROFILE',0,'L');
@@ -50,8 +50,8 @@
 		$this->pdf->MultiCell(0,5,$message_1,1,'L');
 		$this->pdf->Ln(1);
 		$this->pdf->MultiCell(0,5,$message_2,1,'L');
-		$this->pdf->SetFont('Courier','',9);
-		$this->pdf->Text(5,135, $dateTime_printed);
+		$this->pdf->SetFont('Courier','',8);
+		$this->pdf->Text(5,5, $dateTime_printed);
 	    $this->pdf->Output();
 	}
 ?>
--- application/views/registration_successful.php
+++ application/views/registration_successful.php
@@ -1,8 +1,9 @@
+<META HTTP-EQUIV=Refresh CONTENT="10;URL='../../'" />    
 <h1>Registration Successful</h1>
 
 <div id="message">   
     <?php
-        echo "Please proceed to the COMELEC for Account Confirmation.<br /><br />";
-        echo '<a href="../../">Back</a>';
+        echo "Please proceed to the COMELEC for Account Confirmation and PRINTING of Access Details.<br /><br />";
+        //echo '<a href="../../">Back</a>';
     ?>
 </div>
