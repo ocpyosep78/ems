8c2654b967fb283fafa1afd5be8efc2e68530bb8
Chin Tinn Lourence S. Tan <chintinntan1391@gmail.com>
2013-12-15 14:15:46 +0800

    added admin_view, election controller, election_model, search voter, FOC(add party), layout, databas(ems.mwb), set election

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -4,7 +4,7 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <title></title>
-    <link rel="stylesheet" href="<?php echo base_url();?>css/style.css" type="text/css" media="screen" charset="utf-8">
+    <link rel="stylesheet" href="<?php echo base_url();?>css/layout.css" type="text/css" media="screen" charset="utf-8">
     
 </head>
 <body>
