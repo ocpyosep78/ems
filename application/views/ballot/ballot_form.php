1b2c52d29762765ee523e8ed3ff0123fc5b41c62
Chin Tinn Lourence S. Tan <chintinntan1391@gmail.com>
2014-01-13 10:19:02 +0800

    edited ballot for program candidates and ssg

--- application/views/ballot/ballot_form.php
+++ application/views/ballot/ballot_form.php
@@ -46,13 +46,13 @@
 					$candidate_ctr += 1;
 					$acct_id = $acct_id;
 					$candidate_id = $page_view_data[$y]['elect_cand_id'];
-					$radio_name = $position_ssg[$y]['pos_id'];
+					$radio_name = $position_ssg[$x]['pos_id'];
 
 					$candidate = $page_view_data[$y]['acct_fname']." ".$page_view_data[$y]['acct_lname'];
 					$party = $page_view_data[$y]['party_name'];
 
 					echo $tb['tr'];
-					echo '<td>'.form_radio($radio_name, $candidate_id, '', '').$tb['td_'];
+					echo '<td>'.form_radio('vote', $candidate_id, '', '').$tb['td_'];
 					echo form_hidden('acct_id',$acct_id);
 					echo '<td>'.'<img src="../css/images/default.jpg" height="75" width="75">'.$tb['td_'];
 					echo $tb['td'];
@@ -93,18 +93,18 @@
 			for($i=0;$i<count($program_candidates);$i++)
 			{
 
-				if($program_candidates[$i]['pos_id'] == $position_program[$x]['pos_id'])
+				if($program_candidates[$i]['pos_id'] == $position_program[$z]['pos_id'])
 				{	
 					$candidate_ctr += 1;
 					$acct_id = $acct_id;
 					$candidate_id = $program_candidates[$i]['elect_cand_id'];
-					$radio_name = $position_program[$i]['pos_id'];
+					$radio_name = $position_program[$z]['pos_id'];
 
 					$candidate = $program_candidates[$i]['acct_fname']." ".$program_candidates[$i]['acct_lname'];
 					$party = $program_candidates[$i]['party_name'];
 
 					echo $tb['tr'];
-					echo '<td>'.form_radio($radio_name, $candidate_id, '', '').$tb['td_'];
+					echo '<td>'.form_radio('vote', $candidate_id, '', '').$tb['td_'];
 					echo form_hidden('acct_id',$acct_id);
 					echo '<td>'.'<img src="../css/images/default.jpg" height="75" width="75">'.$tb['td_'];
 					echo $tb['td'];
