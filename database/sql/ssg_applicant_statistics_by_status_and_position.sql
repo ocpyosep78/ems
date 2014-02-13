60c4b72d28f9691ca766cc550f881fbcbf03e3fb
lancebogz <fluid_surf17@yahoo.com>
2014-02-13 18:27:53 +0800

    SQL: ssg_applicant _statistics_by_status_and_position_per_program

--- database/sql/ssg_applicant_statistics_by_status_and_position.sql
+++ database/sql/ssg_applicant_statistics_by_status_and_position.sql
@@ -1,15 +1,19 @@
-SELECT 	position.pos_name,
-		SUM(election_candidate.pos_id AND division.div_id = 1 AND election_candidate.status = 0 ) AS Pending,
-		SUM(election_candidate.pos_id AND division.div_id = 1 AND election_candidate.status = 1 ) AS Approved,
-		SUM(election_candidate.pos_id AND division.div_id = 1 AND election_candidate.status = 2 ) AS Rejected,
-		SUM(election_candidate.pos_id AND division.div_id = 1) AS Total
+SELECT position.pos_name,
+SUM(election_candidate.pos_id AND position.div_id = 1 AND election_candidate.status = 0 AND program.prog_id = 1 ) AS Pending,
+SUM(election_candidate.pos_id AND position.div_id = 1 AND election_candidate.status = 1 AND program.prog_id = 1) AS Approved,
+SUM(election_candidate.pos_id AND position.div_id = 1 AND election_candidate.status = 2 AND program.prog_id = 1) AS Rejected,
+SUM(election_candidate.pos_id AND position.div_id = 1 AND program.prog_id = 1) AS Total
 
 FROM position
-RIGHT OUTER JOIN election_candidate ON position.pos_id = election_candidate.pos_id
-LEFT OUTER JOIN division ON position.div_id = division.div_id
-INNER JOIN election ON election_candidate.elect_id = election.elect_id
+
+LEFT OUTER JOIN election_candidate ON position.pos_id = election_candidate.pos_id
+INNER JOIN division ON position.div_id = division.div_id  AND position.div_id = 1
+LEFT OUTER JOIN election ON election_candidate.elect_id = election.elect_id AND election.status = 1
+LEFT OUTER JOIN account ON election_candidate.acct_id = account.acct_id
+LEFT OUTER JOIN course ON account.course_id = course.course_id
+LEFT OUTER JOIN program ON course.prog_id = program.prog_id 
 
 
-WHERE election.status = 1
+
 GROUP BY position.pos_name
-ORDER BY order_no ASC;
+ORDER BY pos_name ASC;
