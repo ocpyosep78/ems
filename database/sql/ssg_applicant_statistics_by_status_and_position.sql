4a59878348e3a4148841d7d2f4895e6c93191f9f
lancebogz <fluid_surf17@yahoo.com>
2014-02-13 18:38:57 +0800

    SQL: ssg_applicant_statistics_by_status_and_position

--- database/sql/ssg_applicant_statistics_by_status_and_position.sql
+++ database/sql/ssg_applicant_statistics_by_status_and_position.sql
@@ -1,19 +1,21 @@
-SELECT position.pos_name,
-SUM(election_candidate.pos_id AND position.div_id = 1 AND election_candidate.status = 0 AND program.prog_id = 1 ) AS Pending,
-SUM(election_candidate.pos_id AND position.div_id = 1 AND election_candidate.status = 1 AND program.prog_id = 1) AS Approved,
-SUM(election_candidate.pos_id AND position.div_id = 1 AND election_candidate.status = 2 AND program.prog_id = 1) AS Rejected,
-SUM(election_candidate.pos_id AND position.div_id = 1 AND program.prog_id = 1) AS Total
+SELECT 	program.prog_id,
+		program.prog_name,
+		SUM(election_candidate.pos_id AND division.div_id = 1 AND election_candidate.status = 0 ) AS Pending,
+		SUM(election_candidate.pos_id AND division.div_id = 1 AND election_candidate.status = 1 ) AS Approved,
+		SUM(election_candidate.pos_id AND division.div_id = 1 AND election_candidate.status = 2 ) AS Rejected,
+		SUM(election_candidate.pos_id AND division.div_id = 1) AS Total
 
-FROM position
+FROM election.program
 
-LEFT OUTER JOIN election_candidate ON position.pos_id = election_candidate.pos_id
-INNER JOIN division ON position.div_id = division.div_id  AND position.div_id = 1
-LEFT OUTER JOIN election ON election_candidate.elect_id = election.elect_id AND election.status = 1
-LEFT OUTER JOIN account ON election_candidate.acct_id = account.acct_id
-LEFT OUTER JOIN course ON account.course_id = course.course_id
-LEFT OUTER JOIN program ON course.prog_id = program.prog_id 
+		
+LEFT OUTER JOIN course ON program.prog_id = course.prog_id
+LEFT OUTER JOIN account ON course.course_id = account.course_id
+LEFT OUTER JOIN election_candidate ON account.acct_id = election_candidate.acct_id
+LEFT OUTER JOIN election ON election_candidate.elect_id = election.elect_id
+LEFT OUTER JOIN position ON election_candidate.pos_id = position.pos_id
+LEFT OUTER JOIN division ON position.div_id = division.div_id
 
 
-
-GROUP BY position.pos_name
-ORDER BY pos_name ASC;
+GROUP BY program.prog_name
+ORDER BY prog_id ASC
+;
