213dea991de0547e2456347731fe1aef99d11dea
lancebogz <fluid_surf17@yahoo.com>
2014-02-13 22:06:58 +0800

    SQL :count_ssg_applicants_by_status_and_position

--- database/sql/ssg_applicant_statistics_by_status.sql
+++
@@ -1,8 +0,0 @@
-SELECT 	election_candidate.status,
-		COUNT(election_candidate.status) AS TOTAL
- 
-FROM election_candidate
-	LEFT OUTER JOIN election ON election_candidate.elect_id = election.elect_id
-
-WHERE election.status = 1
-GROUP BY election_candidate.status;
